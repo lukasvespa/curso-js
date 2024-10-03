aaeb6a0a4d9eb897fa469333c1b051b8af360daa
Lucas Vespa <contato.lucasvespa@gmail.com>
2024-10-02 21:55:23 -0300

    Add files via upload

---
+++ project/index.js
@@ -0,0 +1,34 @@
+const nome = "Tafarel";
+const sobrenome = 'Chicotti';
+const texto = `Colocar o texto`;
+const nomeCompleto = nome + ' ' + sobrenome
+const idade = 32
+const frase = "O nome da pessoa é: " + nomeCompleto + ". E sua idade é de " + idade + " anos"
+const frase2 = `O nome da pessoa é ${nomeCompleto}. E sua idade é ${idade} anos`;
+
+console.log(frase2)
+
+char 
+
+
+
+// Nunca iniciar o nome com um número | var 1nome; (errado)
+// Nunca acentuar a variávei | var profissão; (errado) | correto: var profissao;
+// O único caracter especial aceito é o underline/underscore _ | var _nome; | var nome_profissao;
+// Nunca separar por espaço, utilizar o underlin/underscore | 
+// errado: var primeiro nome;  | correto: var primeiro_nome;
+
+// snake_case: separados por underline/underscore.
+// PascalCase: palavras iniciadas em letras maiúsculas.
+// camelCase: palavras separadas iniciadas em letras maiúsculas.
+
+// addEventListener = add event listener
+// python add_event_listener
+
+// ECMAScript 
+//var -> variáveis que são varia o valor
+// const -> variáveis constantes, que o valor nunca mudará
+// let -> variáveis que podem mudar e serão descartadas
+// node index.js
+// cls - clear screen # Windows - CMD-DOS/Powershell
+// clear - # Linux / MacOS - Bash
---
+++ project/index2.js
@@ -0,0 +1,6 @@
+var anoatual = 2024;
+var anonasc = 2003;
+var idade = (anoatual - anonasc); 
+
+
+console.log(`sua idade é de  ${idade}`);
