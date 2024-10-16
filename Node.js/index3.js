ef606f558b0d7d128332dda9c484e70fb4e59d0a
Lucas Vespa <contato.lucasvespa@gmail.com>
2024-10-16 20:05:18 -0300

    Add files via upload

---
+++ Node.js/index3.js
@@ -0,0 +1,11 @@
+// Laços de Repetição
+// FOR --- Para
+
+for(let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+// WHILE --- Enquanto
+// DO WHILE -- Faça enquanto
+
+//npm install readline-sync
