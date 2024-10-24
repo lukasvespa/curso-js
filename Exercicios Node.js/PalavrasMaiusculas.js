6f7e52df44531f231af4dac300772e4b2cef8201
Lucas Vespa <contato.lucasvespa@gmail.com>
2024-10-23 21:45:26 -0300

    Add files via upload

---
+++ Exercicios Node.js/PalavrasMaiusculas.js
@@ -0,0 +1,6 @@
+// Converta os elementos da lista abaixo, para letra maiúscula (toUpperCase)
+const palavras = ["bola", "tênis", "carro", "celular", "computador"];
+
+const palavrasMaiusculas = palavras.map( palavra => palavra.toUpperCase() )
+
+console.log(palavrasMaiusculas)
